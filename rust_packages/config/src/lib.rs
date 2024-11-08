82ace60f6125403c4e3b405edee061caee679b25
Json_Lee <2622336659@qq.com>
2024-11-08 16:21:37 +0800

    feat: 新增config包通过pathBuff去读取

--- rust_packages/config/src/lib.rs
+++ rust_packages/config/src/lib.rs
@@ -13,8 +13,7 @@ use serde::de::DeserializeOwned;
 pub use enums::EnvMode;
 pub use file::{__dirname, resolve};
 
-pub fn load(base_path: &str) -> Result<Config> {
-    let root_path = resolve(&__dirname()?, base_path)?;
+pub fn load_from_path(base_path: &PathBuf) -> Result<Config> {
     let env_str = env::var("ENV_MODE").unwrap_or_else(|_| "dev".into());
     let file_type = env::var("CONFIG_FILE_TYPE").unwrap_or_else(|_| "toml".into());
     let file_name = env::var("CONFIG_FILE_NAME").unwrap_or_else(|_| "config".into());
@@ -26,7 +25,7 @@ pub fn load(base_path: &str) -> Result<Config> {
         format!("{}.{}.local", file_name.as_str(), env.as_str()),
     ]
     .iter()
-    .filter_map(|item| resolve(&root_path, format!("{}.{}", item, file_type).as_str()).ok())
+    .filter_map(|item| resolve(base_path, format!("{}.{}", item, file_type).as_str()).ok())
     .filter(|item| item.exists())
     .collect();
     if config_paths.is_empty() {
@@ -35,7 +34,6 @@ pub fn load(base_path: &str) -> Result<Config> {
             "No configuration file is currently found",
         ));
     }
-
     let mut config_builder = Config::builder();
 
     for path in config_paths {
@@ -48,15 +46,27 @@ pub fn load(base_path: &str) -> Result<Config> {
     Ok(config)
 }
 
+pub fn load(base_path: &str) -> Result<Config> {
+    let root_path = resolve(&__dirname()?, base_path)?;
+    load_from_path(&root_path)
+}
+
 pub fn load_as<T>(base_path: &str) -> Result<T>
 where
     T: DeserializeOwned,
 {
     let config_instance = load(base_path)?;
-    config_instance.try_deserialize().map_err(|e| {
-        Error::new(
-            ErrorKind::Other,
-            format!("Deserialize config error: {}", e),
-        )
-    })
+    config_instance
+        .try_deserialize()
+        .map_err(|e| Error::new(ErrorKind::Other, format!("Deserialize config error: {}", e)))
+}
+
+pub fn load_from_path_as<T>(base_path: &PathBuf) -> Result<T>
+where
+    T: DeserializeOwned,
+{
+    let config_instance = load_from_path(base_path)?;
+    config_instance
+        .try_deserialize()
+        .map_err(|e| Error::new(ErrorKind::Other, format!("Deserialize config error: {}", e)))
 }
